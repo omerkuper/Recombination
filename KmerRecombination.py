9c0bf185c77f275617e1ff8dfeb7c6e1a6140ead
omerkuper <40544034+omerkuper@users.noreply.github.com>
2020-04-25 20:17:24 +0300

    Update KmerRecombination.py

--- KmerRecombination.py
+++ KmerRecombination.py
@@ -2,33 +2,28 @@ ls = ['A', 'T', 'C', 'G']
 matrix_db = [ls[i] + ls[y] for i in range(len(ls)) for y in range(len(ls))]
 
 
-def match(data_seq, seq):
-    return [outer + inner for outer in data_seq for inner in seq]
-
-
 class recombination:
-    def __init__(self, k_mer):
+    def __init__(self, k_mer, inx=None):
         self.k_mer = k_mer // 2
         self.flt_pt = k_mer % 2
         self.data_seq = matrix_db
         self.x = 2 if self.flt_pt == 0 else 1
+        self.inx = inx
 
-    def look(self, count=0, inx=None):
-        inx, counter = inx, count
+    def look(self, counter=0):
         if counter <= self.k_mer - self.x:
             if counter < self.k_mer - 1:
-                self.data_seq = match(self.data_seq, matrix_db)[: inx]
+                self.data_seq = [outer + inner for outer in self.data_seq for inner in matrix_db][: self.inx]
             else:
-                self.data_seq = match(self.data_seq, ls)[: inx]
+                self.data_seq = [outer + inner for outer in self.data_seq for inner in ls][: self.inx]
             counter += 1
-            return self.look(counter, inx)
+            return self.look(counter)
         else:
             return self.data_seq
 
 
-k_mer = 10
+k_mer = 13
 run = recombination(k_mer)
 result = run.look()
 print(result)
 print(len(result))
-
